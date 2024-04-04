26db01022daa712f4287b3f353ec3c722590d374
daffa110404 <dd8195813@gmail.com>
2024-04-04 19:53:40 +0800

    Modul4RektaCodelab

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.modul4rektacodelab {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.modul4rektacodelab to javafx.fxml;
+    exports com.example.modul4rektacodelab;
+}
